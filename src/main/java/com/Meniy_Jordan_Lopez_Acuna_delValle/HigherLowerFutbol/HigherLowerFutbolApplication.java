186b8f19433bd6d9d872c893f7e292a871548394
Federico Meniy <federicomeniy@gmail.com>
2025-05-31 15:25:31 -0300

    Higher Lower Futbol

---
+++ src/main/java/com/Meniy_Jordan_Lopez_Acuna_delValle/HigherLowerFutbol/HigherLowerFutbolApplication.java
@@ -0,0 +1,13 @@
+package com.Meniy_Jordan_Lopez_Acuna_delValle.HigherLowerFutbol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HigherLowerFutbolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HigherLowerFutbolApplication.class, args);
+	}
+
+}
