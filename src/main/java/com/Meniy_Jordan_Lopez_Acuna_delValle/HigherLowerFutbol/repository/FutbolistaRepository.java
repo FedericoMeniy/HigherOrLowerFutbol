49c195e76325f297b8bc253962e9831202d31459
Federico Meniy <99376685+FedericoMeniy@users.noreply.github.com>
2025-06-13 17:25:47 -0300

    Delete src directory

--- src/main/java/com/Meniy_Jordan_Lopez_Acuna_delValle/HigherLowerFutbol/repository/FutbolistaRepository.java
+++
@@ -1,8 +0,0 @@
-package com.Meniy_Jordan_Lopez_Acuna_delValle.HigherLowerFutbol.repository;
-
-import com.Meniy_Jordan_Lopez_Acuna_delValle.HigherLowerFutbol.entity.Futbolista;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FutbolistaRepository extends JpaRepository<Futbolista, Long> {
-
-}
--- src/main/java/com/Meniy_Jordan_Lopez_Acuna_delValle/HigherLowerFutbol/service/FutbolistaDataSyncService.java
+++
@@ -1,5 +0,0 @@
-package com.Meniy_Jordan_Lopez_Acuna_delValle.HigherLowerFutbol.service;
-
-public class FutbolistaDataSyncService {
-    //
-}
